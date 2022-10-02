c620868b6428adb9348a2c48018c7f0aafa86f19
davee8k <davee8k@users.noreply.github.com>
2022-10-02 18:48:44 +0200

    Documentation updates

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 ## Description
 
-TODO
+Simple class for tabs.
 
 ## Options
+
+Name        | Type       | Default    | Description
+:---------- | :--------- | :--------- | :-----------
+activeClass | String     | active     | class of the selected item and tab
+tabs        | String     | li         | parent element to which the active class will be assigned, or null for the selected link
+open        | Boolean    | true       | disable automatic opening of the tab based on the current hash
+
+## Usage
+
+```javascript
+$("#target").tabs({});
+```
--- src/tabs.js
+++ src/tabs.js
@@ -2,29 +2,41 @@
  * Jquery Tabs
  *
  * @author DaVee8k
- * @version 0.1.0
+ * @version 0.1.1
  * @license WTFNMFPL 1.0
  */
 (function ($) {
 	$.fn.tabs = function (option) {
 		var self = this;
-		var tabPanels = $(self).find("li");
-		if (tabPanels.length === 0) return 0;
+		this.ac = option['activeClass'] !== undefined ? option['activeClass'] : "active";
+		this.tabs = option['tabs'] !== undefined ? option['tab'] : "li";
+		if (this.tabs && $(self).find(this.tabs).length === 0) return 0;
 
+		/**
+		 * Change active tab and displayed block
+		 * @param {DOMelement} link
+		 */
 		this.changeTab = function (link) {
-			$(link).parent().siblings().removeClass("active");
-			$(link).parent().addClass("active");
+			if (this.tabs) {
+				var item = $(link).closest(this.tabs);
+				$(item).siblings().removeClass(this.ac);
+				$(item).addClass(this.ac);
+			}
+			else {
+				$(this).find("a").removeClass(this.ac);
+				$(link).addClass(this.ac);
+			}
 			var tab = $(link).attr("href");
-			$(tab).siblings().removeClass("active");
-			$(tab).addClass("active");
-		}
+			$(tab).siblings().removeClass(this.ac);
+			$(tab).addClass(this.ac);
+		};
 
 		$(this).find("a").click(function (e) {
 			self.changeTab(this);
 			return false;
 		});
 
-		if (option["open"] !== false && window.location.hash) {
+		if (option['open'] !== false && window.location.hash) {
 			$(this).find("a").each(function () {
 				if (this.hash == window.location.hash) return self.changeTab(this);
 			});
